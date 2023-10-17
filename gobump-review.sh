0f7ba64aae7215a5155945b689c0197cf559f2b4
Jingyuan Liang <jingyuanliang@google.com>
2023-10-17 17:46:13 +0000

    GOBUMP_REVIEW_MESSAGE

--- gobump-review.sh
+++ gobump-review.sh
@@ -22,4 +22,7 @@ push_options=()
 for reviewer in ${GOBUMP_REVIEW_REVIEWERS:-}; do
   push_options+=(-o "r=${reviewer}")
 done
+if [[ -n "${GOBUMP_REVIEW_MESSAGE:-}" ]]; then
+  push_options+=(-o "m=${GOBUMP_REVIEW_MESSAGE:-}")
+fi
 git push "${GOBUMP_REVIEW_REMOTE:-origin}" "HEAD:refs/for/${GOBUMP_REVIEW_BRANCH:-main}" "${push_options[@]}"
